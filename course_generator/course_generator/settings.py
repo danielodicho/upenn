fc9d9427ae7ba5904c8a2a465e88f1e3cc8c6a33
danielodicho <danielodicho@gmail.com>
2024-09-22 06:04:58 -0400

    course generating code, fuck

--- course_generator/course_generator/settings.py
+++ course_generator/course_generator/settings.py
@@ -35,6 +35,13 @@ CACHES = {
         'LOCATION': 'unique-snowflake',
     }
 }
+# settings.py
+
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
 
 
 INSTALLED_APPS = [
